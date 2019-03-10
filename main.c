9e3241f0a9b7a5608e57cbb37dbfa84e47e80e3d
shadlyd15 <shadlyd15@gmail.com>
2019-03-10 17:29:13 +0600

    Example Added

--- main.c
+++ main.c
@@ -3,8 +3,39 @@
 #include <stdint.h>
 #include "debug.h"
 
+typedef struct A_t{
+	char name[64];
+}A_t;
+
+typedef struct B_t{
+	char name[64];
+	int data;
+}B_t;
+
+typedef union{
+	A_t A;
+	B_t B;
+	struct{
+		char name[64];
+	};
+} abstract_interface_t;
 
 int main(int argc, char const *argv[]){
 	DEBUG_OK("Polymorphic Interface Test In C");
+	
+	A_t A = {0};
+	memcpy(A.name, "A", strlen("A"));
+	
+	B_t B = {0};
+	B.data = 100;
+	memcpy(B.name, "B", strlen("B"));
+
+	abstract_interface_t abstract_interface = {0};
+	memcpy(abstract_interface.name, "abstract_interface", strlen("abstract_interface"));
+	
+	DEBUG_OK("A : %s", A.name);
+	DEBUG_OK("B : %s", B.name);
+	DEBUG_OK("B.data : %d", B.data);
+	DEBUG_OK("abstract_interface : %s", abstract_interface.name);
 	return 0;
 }
